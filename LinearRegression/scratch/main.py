538d22c4cad8e5f42bb3b7212be3d4cde2b588f9
Rishav Bhagat <rishavb123@gmail.com>
2018-07-14 17:56:19 -0400

    Added import random

--- LinearRegression/scratch/main.py
+++ LinearRegression/scratch/main.py
@@ -5,8 +5,25 @@ import random
 
 style.use('fivethirtyeight')
 
-xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
-ys = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
+# xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
+# ys = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
+
+
+def create_dataset(num_of_datapoints, variance, step=2, correlation=''):
+    val = 1
+    y_arr = []
+    for i in range(num_of_datapoints):
+        if variance != 0:
+            y_arr.append(val + random.randrange(-variance, variance))
+        else:
+            y_arr.append(val)
+
+        if correlation and correlation == 'pos':
+            val += step
+        elif correlation and correlation == 'neg':
+            val -= step
+    x_arr = [i for i in range(num_of_datapoints)]
+    return np.array(x_arr, dtype=np.float64), np.array(y_arr, dtype=np.float64)
 
 
 def mean(arr):
@@ -36,6 +53,8 @@ def coefficient_of_determination(ys_orig, ys_line):
     return 1 - (squared_error_regr / squared_error_mean)
 
 
+xs, ys = create_dataset(40, 10, 2, correlation='pos')
+
 m = best_fit_slope(xs, ys)
 b = best_fit_intercept(xs, ys, m)
 
@@ -46,5 +65,5 @@ print r_squared
 
 plt.scatter(xs, ys)
 plt.plot(xs, regression_line)
-plt.scatter(8, predict(8, m, b), color='g')
+plt.scatter(42, predict(42, m, b), s=100, color='g')
 plt.show()
